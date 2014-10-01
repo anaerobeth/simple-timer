423bcb1bc1b941df6533cc93f55a989c5cbbcae5
Beth Tenorio <betsky98@gmail.com>
2014-09-30 23:34:41 -0400

    calculate the total time logged and remaining for each month

--- time_summary.rb
+++ time_summary.rb
@@ -1,12 +1,25 @@
-puts 'Enter month to report'
+puts '-----------------------------------------------------'
+puts 'Enter month number to report'
 month = gets.chomp
+
 filename = "timesheet-#{month}.txt"
-nums = []
-File.open(filename, 'r').each_line do |log|
-  nums << log.gsub(/Logged /,'').gsub(/minutes.*/,'').to_i
+
+WEEKLY_HOURS = 12
+
+if File.file?(filename)
+  minutes = []
+  File.open(filename, 'r').each_line do |log|
+    minutes << log.gsub(/Logged /,'').gsub(/minutes.*/,'').to_i
+  end
+
+  target_minutes_per_month = WEEKLY_HOURS * 60 * 4.3
+
+  total_minutes = minutes.reduce(:+)
+  puts ''
+  puts "Total time for month #{month} is #{total_minutes} minutes"
+  puts "Remaining time is #{(target_minutes_per_month - total_minutes).round(0)} minutes"
+  puts '-----------------------------------------------------'
+else
+  puts 'You do not have time logged for that month'
 end
 
-target = 12 * 60 * 4.3
-total = nums.reduce(:+)
-puts "Total time for month #{month} is #{total} minutes"
-puts "Remaining time is #{(target - total).round(0)} minutes"
